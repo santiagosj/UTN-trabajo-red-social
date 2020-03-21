3dd76c83d19e853e69c0c5cd0694376e118b231d
Santiago Spinetto Jung <sanjs965@gmail.com>
2020-03-21 20:42:25 -0300

    second-commit

--- src/components/AppHolder/AppHolder.js
+++ src/components/AppHolder/AppHolder.js
@@ -9,7 +9,8 @@ const AppHolder = () => {
   useEffect(() => {
     async function getProfiles(){
       const result = await axios(
-        'http://localhost:8001/api/profiles',
+        //'http://localhost:8001/api/profiles',
+        'https://firebasestorage.googleapis.com/v0/b/reactappdemo-utn-unidad-4.appspot.com/o/profiles.json?alt=media&token=8ef37ed3-d280-4e25-906d-d1bc908b1319'
       );
       setData(result.data);
     }
