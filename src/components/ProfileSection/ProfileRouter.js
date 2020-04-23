b6db957bb5b35386196a96a0abbdd472f755bafd
Santiago Spinetto Jung <sanjs965@gmail.com>
2020-04-23 13:37:20 -0300

    dovenv :pray:

--- src/components/ProfileSection/ProfileRouter.js
+++ src/components/ProfileSection/ProfileRouter.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-//import ProfileList from './ProfileList'
 import Gente from '../../pages/Gente'
 import ProfileDetail from '../../pages/ProfileDetail'
-
-// The Roster component matches one of two different routes
-// depending on the full pathname
 
 /**
  * --------------
